f54635e12944306a80403173abc73f8ff1b3a9cc
JaKrau <jakeziah@gmail.com>
2023-07-03 10:54:58 -0700

    working on localStorage

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -22,6 +22,14 @@ $(function () {
   // TODO: Add code to display the current date in the header of the page.
 
 });
+
+class TimeBlock{
+  constructor(hour, event){
+    this.hour = hour;
+    this.event = event;
+  }
+}
+
 let today = dayjs();
 let currentDay = document.getElementById('currentDay');
 currentDay.textContent = today.format('MMMM DD, YYYY');
@@ -47,6 +55,7 @@ for (let hourIndex=0; hourIndex < hoursInDay; hourIndex++) {
   let time24 = hours[hourIndex].twentyFourHourIndex;
   let timeDisplay = hours[hourIndex].time + hours[hourIndex].text;
   let currentTime24 = dayjs().format('H');
+  
   let hourSection = document.createElement("section");
   hourSection.className = 'row time-block';
   hourSection.id = "hour-" + hours[hourIndex].time;
@@ -58,28 +67,82 @@ for (let hourIndex=0; hourIndex < hoursInDay; hourIndex++) {
   let textArea = document.createElement('textarea');
   textArea.className = "col-8 col-md-10 description";
   textArea.rows = '3';
-  
+
+  function addEvent() {
+    myParent = $(this).parent();
+    myIndex = hours[hourIndex].twentyFourHourIndex;
+    note = myParent.children("textarea").val();
+    //localStorage.setItem('note', note);
+    saveEvent(myIndex, note);
+  }
   let saveButton = document.createElement('button');
   saveButton.className = 'btn saveBtn col-2 col-md-1';
   saveButton.setAttribute('aria-label', 'save');
-  
+  $(".saveBtn").on("click", addEvent);
+/*
+  $(".saveBtn").on("click", function () {
+    var myParent = $(this).parent();
+    var hour = myParent.data("hour");
+    var event = myParent.children("textarea").val();
+    saveEvent(hour, event);
+  });
+*/
   let iElement = document.createElement('i');
   iElement.className = 'fas fa-save';
   iElement.setAttribute('aria-hidden', 'true');
+
   saveButton.appendChild(iElement);
+  //textArea.appendChild(saveButton);
   hourSection.appendChild(hourDiv); 
   hourSection.appendChild(textArea);
   hourSection.appendChild(saveButton);
   htmlMain.appendChild(hourSection);
   
   if (time24 < currentTime24){
-    textArea.className += "";
-    textArea.className += "col-8 col-md-10 description past";
+    textArea.className += " past";
   } else if (time24 == currentTime24) {
-    textArea.className += "";
-    textArea.className += "col-8 col-md-10 description present";
+    textArea.className += " present";
   } else if (time24 > currentTime24) {
-    textArea.className += "";
-    textArea.className += "col-8 col-md-10 description future";
+    textArea.className += " future";
   }
+
+  //function saveEvent(hour, event){
+  //  var eventData = [today.format("MM/DD/YYYY"), event];
+  //  localStorage.setItem("timeBlock-" + hour.toString(), JSON.stringify(eventData));
+  //}
+  function saveEvent(myIndex, note){
+    var noteDate = [today.format("MM/DD/YYYY"), note];
+    localStorage.setItem(myIndex.toString(), JSON.stringify(noteDate));
+  }
+  function getEvent(){
+    storedEvent = localStorage.getItem('note');
+    if (storedEvent === null) {
+      return '';
+    } else {
+      textArea.textContent = storedEvent;
+    }
+  }
+  
+  //function getEvent(hour){
+  //  var event = JSON.parse(localStorage.getItem("timeBlock-" + hour.toString()));
+  //  if(event === null){
+  //    return "";
+  //  }
+  //  if(event[0] != today.format("MM/DD/YYYY")){
+  //    clearEvent(hour);
+  //    return "";
+  //  }
+  //  return event[1];
+  //}
+
+  //function clearEvent(hour){
+  //  localStorage.removeItem("timeBlock-" + hour.toString());
+  //}
+
+  getEvent();
+}
+
+function clearSchedule(){
+  localStorage.clear();
+
 }
